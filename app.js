59d0dfb9feb5a31625bddad18da141551a4444bd
edensalt <124738007+edensalt@users.noreply.github.com>
2023-05-03 11:07:27 -0500

    Fix diagonal to bottom right win logic

--- app.js
+++ app.js
@@ -232,7 +232,7 @@ const gameController = (function () {
 
     if (
       board[2][0] === board[1][1] &&
-      board[1][1] === board[2][0] &&
+      board[1][1] === board[0][2] &&
       board[1][1] !== ""
     ) {
       displayWinner();
